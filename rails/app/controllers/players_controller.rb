5ff2fa6301af95d8cb188229795308adb3c308a2
Jukka Kaartinen <jkaartinen@gmail.com>
2010-02-16 22:34:59 +0200

    team model added. new and edit views added partly to player.

--- rails/app/controllers/players_controller.rb
+++ rails/app/controllers/players_controller.rb
@@ -1,18 +1,55 @@
 class PlayersController < ApplicationController
-  def index
-    @person = Person.find(params[:person_id])
-    @players = @person.players
-  end
 
-  def show
-    @person = Person.find(params[:person_id])
-    @players = @person.players.find(params[:id])
+  def index 
+    @person = Person.find(params[:person_id])  
+    @players = @person.players 
+  end  
+   
+  def show 
+    @person = Person.find(params[:person_id])  
+    @player = @person.players.find(params[:id])  
+  end  
+  
+  def new 
+    @person = Person.find(params[:person_id]) 
+    @player = 
+    @person.players.build 
+  end  
+    
+  def create 
+    @person = Person.find(params[:person_id])  
+    @player = @person.players.build(params[:player])  
+    
+    if @player.save 
+      redirect_to person_player_url(@person, @player)  
+    else  
+      render :action => "new"  
+    end
+  end  
+   
+  def edit 
+    @person = Person.find(params[:person_id])  
+    @player = @person.players.find(params[:id])  
+  end  
+   
+  def update 
+    @person = Person.find(params[:person_id])  
+    @player = Player.find(params[:id])  
+   
+    if @player.update_attributes(params[:player])  
+      redirect_to person_player_url(@person, @player) 
+    else  
+      render :action => "edit"  
+    end  
+  end  
+    
+  def destroy 
+    @person = Person.find(params[:person_id])  
+    @player = Player.find(params[:id])  
+    @player.destroy respond_to do |format| 
+      format.html { redirect_to person_players_path(@person) } 
+      format.xml { head :ok } 
+    end  
   end
-
-  def new
-  end
-
-  def edit
-  end
-
-end
+ 
+end 
