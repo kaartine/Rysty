cf358c08cc4329f90f35dcaeb2c57bbf1e123367
Jukka Kaartinen <jkaartinen@gmail.com>
2011-01-31 21:43:44 +0200

    *bug fix for situation when there are no contests at all
    Signed-off-by: Jukka Kaartinen <jkaartinen@gmail.com>

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,9 +1,9 @@
 class ContestsController < ApplicationController
-  
+
   def index
-    @season = APP_CONFIG["current_season"] if @season.nil?        
+    @season = APP_CONFIG["current_season"] if @season.nil?
     @season = params[:season] if !params[:season].nil?
-      
+
     if params[:contest].nil? && session[:user_contest_id].nil?
       @contest = Contest.first(:conditions => ["season = ?",@season], :include => :teams)
     elsif !params[:contest].nil?
@@ -11,27 +11,29 @@ class ContestsController < ApplicationController
     else
       @contest = Contest.find(session[:user_contest_id], :include => :teams)
     end
-    
-    session[:user_contest_id] = @contest.id
-     
-    calculate_team_points(@contest.id)
-    
-    @games = Game.all(:conditions => ['contest_id = ? AND played <> true', @contest.id], :limit => '5')
+
+    unless @contest.nil?
+      session[:user_contest_id] = @contest.id
+
+      calculate_team_points(@contest.id)
+
+      @games = Game.all(:conditions => ['contest_id = ? AND played <> true', @contest.id], :limit => '5')
+    end
   end
-  
+
   def show
     @contest = Contest.find(params[:id], :include => :teams)
     calculate_team_points(params[:id])
-      
+
     session[:user_contest_id] = params[:id]
   end
-  
+
   private
   def calculate_team_points(contest_id)
     for team in @contest.teams
       team.calculate_points(contest_id, team.id)
     end
-    
-    @contest.teams.sort! { |a, b| b.points <=> a.points }    
+
+    @contest.teams.sort! { |a, b| b.points <=> a.points }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -148,7 +149,7 @@ ActiveRecord::Schema.define(:version => 28) do
     t.datetime "updated_at"
   end
 
-  add_index "followed_contests", ["contest_id", "user_id"], :name => "index_followed_contests_on_user_id_and_contest_id", :unique => true
+  add_index "followed_contests", ["user_id", "contest_id"], :name => "index_followed_contests_on_user_id_and_contest_id", :unique => true
 
   create_table "game_photos", :force => true do |t|
     t.integer  "game_id"
@@ -181,6 +182,7 @@ ActiveRecord::Schema.define(:version => 28) do
     t.integer  "guest_team_id"
     t.integer  "winner_id"
     t.boolean  "draw"
+    t.boolean  "played"
     t.integer  "contest_id"
     t.datetime "when"
     t.integer  "home_goals"
